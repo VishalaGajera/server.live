01ac32faf6a67916ed961913ae01daba161e9a55
VishalaGajera <vishalagajera@gmail.com>
2025-10-15 21:41:01 +0530

    change in mailservice file

--- mailServices.js
+++ mailServices.js
@@ -1,11 +1,7 @@
 const nodemailer = require("nodemailer");
-const { Resend } = require("resend");
 
 const username = process.env.GMAIL_USERNAME;
 const password = process.env.GMAIL_PASSWORD;
-const resedApiKey=process.env.RESEND_API_KEY;
-const resend = new Resend(resedApiKey);
-
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
@@ -22,13 +18,11 @@ const SendMailToApplicient = async (from, Subject, html, to) => {
       subject: Subject,
       html: html,
     };
-    console.log("MailOption :", MailOption);
-    // const info = await transporter.sendMail(MailOption);
-    const info = await resend.emails.send(MailOption);
+    const info = await transporter.sendMail(MailOption);
     console.log("info :", info);
     return { success: true, message: "Email sent successfully" };
   } catch (error) {
-    console.log(`error in sending email: ${error}`);
+    console.error(error);
     return { success: false, message: "Error sending email" };
   }
 };
